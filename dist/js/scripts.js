351df7ddc65e858071245c24fbcb13b3737ff56f
Neil Dewing <neildewing@Neils-MacBook-Pro.local>
2019-05-27 15:16:59 +0100

    dev

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,36 @@
 (function(window, document) {
-    $(document).ready(function() {});
+    var lpPrefix = "hmlp";
+    function stickyNav() {
+        var currentScroll, quickNavTop;
+        quickNavTop = $("." + lpPrefix + "-nav-wrap").offset().top;
+        function doSticky() {
+            currentScroll = $(window).scrollTop();
+            if (currentScroll >= quickNavTop) {
+                $("body").addClass(lpPrefix + "-sticky-nav--fixed");
+            } else {
+                $("body").removeClass(lpPrefix + "-sticky-nav--fixed");
+            }
+        }
+        $(window).scroll(doSticky);
+        doSticky();
+    }
+    function quickNavScroll() {
+        $(".js-" + lpPrefix + "-scrollto").click(function(e) {
+            e.preventDefault();
+            var anchor, offset;
+            anchor = $(this).attr("href");
+            offset = $("." + lpPrefix + "-nav-wrap").outerHeight() - 1;
+            $("html, body").stop().animate({
+                scrollTop: $(anchor).offset().top - offset
+            }, 1500, "easeInOutExpo");
+            console.info(offset);
+        });
+    }
+    function init() {
+        stickyNav();
+        quickNavScroll();
+    }
+    $(document).ready(function() {
+        init();
+    });
 })(window, document);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,68 @@
 (function(window, document){
 
+    const lpPrefix = 'hmlp';
+
+
+
+    //== Sticky Nav (Mobile only)
+
+    function stickyNav() {
+
+        let currentScroll, quickNavTop;
+
+        quickNavTop = $('.'+lpPrefix+'-nav-wrap').offset().top; 
+
+        function doSticky() {
+            currentScroll = $(window).scrollTop();
+            if (currentScroll >= quickNavTop) {                    
+                $('body').addClass(lpPrefix+'-sticky-nav--fixed');                 
+            } else {
+                $('body').removeClass(lpPrefix+'-sticky-nav--fixed');
+            }
+        };
+
+        $(window).scroll(doSticky);
+    
+        // Check on load
+        doSticky();
+
+    }
+
+
+
+    //== Quick Nav Scroll
+
+    function quickNavScroll() {
+        $('.js-'+lpPrefix+'-scrollto').click(function(e){
+            e.preventDefault();
+            let anchor, offset;
+
+            anchor = $(this).attr("href");
+            offset = $('.'+lpPrefix+'-nav-wrap').outerHeight() - 1;
+
+            $('html, body').stop().animate({
+                scrollTop: $(anchor).offset().top - offset
+            }, 1500, 'easeInOutExpo');
+
+            console.info(offset);
+
+        });
+    };
+
+
+    //== Init all functions
+
+    function init() {
+        stickyNav();
+        quickNavScroll();
+    };
+
+
     $(document).ready(function(){
-
-
+    
+        init();
+    
     });
+
 
 })(window, document);
