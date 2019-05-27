f212445bfc82872faa1e9e07b3e8c47f5864dae6
Neil Dewing <neildewing@Neils-MacBook-Pro.local>
2019-05-27 16:00:29 +0100

    quick add initial setup

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -36,10 +36,45 @@
             $(this).parents(".hmlp-modal-wrap").fadeOut(200);
         });
     }
+    function addToBasket() {
+        $(document).on("click", ".js-quickadd", function(e) {
+            var self = $(this);
+            var _dynSessConf = $("input[name='_dynSessConf']").attr("value");
+            var skuID = $(this).attr("data-sku");
+            var prodId = $(this).attr("data-product-id");
+            var data = {
+                _dyncharset: "UTF-8",
+                _dynSessConf: _dynSessConf,
+                productId: prodId,
+                skuId: skuID,
+                quantity: 1
+            };
+            self.html("").addClass(lpPrefix + "-quick-add--in-progress");
+            $.ajax({
+                url: "/modules/ajax/addItemToOrder.jsp",
+                method: "POST",
+                data: $.param(data),
+                success: function success(result) {
+                    self.removeClass(lpPrefix + "-quick-add--in-progress").text("Added");
+                }
+            }).then(function() {
+                $.ajax({
+                    url: "/modules/ajax/basket-masthead.jsp",
+                    success: function success(result) {
+                        $(".js-header-basket-link").html(result);
+                        setTimeout(function() {
+                            self.text("Quick Add");
+                        }, 1e3);
+                    }
+                });
+            });
+        });
+    }
     function init() {
         stickyNav();
         quickNavScroll();
         freeSampleModal();
+        addToBasket();
     }
     $(document).ready(function() {
         init();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -71,12 +71,62 @@
 
 
 
+    //== Add to basket
+    function addToBasket() {
+    
+        $(document).on('click', '.js-quickadd', function(e){
+            var self = $(this);
+        
+            // Variables
+            var _dynSessConf = $("input[name='_dynSessConf']").attr('value');
+            var skuID = $(this).attr('data-sku');
+            var prodId = $(this).attr('data-product-id');
+        
+            // Form Data
+            var data = {
+                '_dyncharset': 'UTF-8',
+                '_dynSessConf': _dynSessConf,
+                'productId' : prodId,
+                'skuId' : skuID,
+                'quantity' : 1
+            };
+        
+            // Show loading spinner and remove text
+            self.html('').addClass(lpPrefix+'-quick-add--in-progress');
+        
+            // Run AJAX call to dyanmically add to basket 
+            $.ajax({
+                url:"/modules/ajax/addItemToOrder.jsp",
+                method:"POST",
+                data: $.param(data),
+                success: function(result){
+                    self.removeClass(lpPrefix+'-quick-add--in-progress').text('Added');
+                }                
+            }).then(function(){
+                $.ajax({
+                    url: '/modules/ajax/basket-masthead.jsp',
+                    success: function(result){
+                        $('.js-header-basket-link').html(result);
+                        setTimeout(function(){
+                            self.text('Quick Add');
+                        }, 1000);
+                    }
+                });
+            });
+        
+        });   
+    };
+
+
+
+
     //== Init all functions
 
     function init() {
         stickyNav();
         quickNavScroll();
         freeSampleModal();
+        addToBasket();
     };
 
 
